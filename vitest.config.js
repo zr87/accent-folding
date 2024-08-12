d8b3239656e2cdf73f123aa99e34efd707f839b6
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-08-12 02:57:34 +0200

    chore: fix vitest config file exclusions

---
+++ vitest.config.js
@@ -0,0 +1,21 @@
+import { defineConfig, configDefaults } from 'vitest/config'
+
+export default defineConfig({
+	test: {
+
+		coverage: {
+			reporter: ['text', 'json', 'json-summary', 'lcov'],
+			exclude:[
+				...configDefaults.exclude,
+				'**/index.*',
+				'release.config.cjs'
+			],
+			thresholds:{
+				lines: 80,
+				functions: 80,
+				branches: 80,
+				statements: 80
+			}
+		},
+	},
+})
