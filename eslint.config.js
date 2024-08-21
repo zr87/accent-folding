7cb65b6e2067aaf894f79e0bf910ab5f1c1ef2a8
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-08-21 14:23:31 +0200

    chore: make Prettier work with ESlint # Let's pause for a bit... our Prettier configuration is now all set and should be working, but there is a hidden problem. ESLint comes with a few default formatting rules out of the box that may collide with Prettier formatting rules. So, to get around it, we need to override those specific ESLint rules in favor of Prettier because Prettier deals with all formatting rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
 	{
@@ -12,6 +13,7 @@ export default [
 		},
 	},
 	pluginJs.configs.recommended,
+	eslintConfigPrettier,
 	{
 		rules: {
 			'no-unused-vars': 'warn',
